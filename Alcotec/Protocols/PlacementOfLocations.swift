94501d81ae2b3c967b9946d2f235e4368cf985a3
YauheniKorp <ayonamirei1@gmail.com>
2020-11-03 00:25:18 +0300

    adding placement of location after starting app

---
+++ Alcotec/Protocols/PlacementOfLocations.swift
@@ -0,0 +1,22 @@
+//
+//  PlacementOfLocations.swift
+//  Alcotec
+//
+//  Created by Admin on 02.11.2020.
+//
+
+import Foundation
+import GoogleMaps
+import GooglePlaces
+
+protocol PlacementOfLocations: SetMarkerProtocol {
+    func placementOfLocation(_ locations: [Location], _ mapView: GMSMapView)
+}
+
+extension PlacementOfLocations {
+    func placementOfLocation(_ locations: [Location], _ mapView: GMSMapView) {
+        for value in locations {
+            createAndSetMarker(CLLocationCoordinate2D(latitude: CLLocationDegrees( value.latitude), longitude: CLLocationDegrees(value.longitude)), value.name, mapView, value.color)
+        }
+    }
+}
--- Alcotec/ViewController.swift
+++ Alcotec/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
-class ViewController: UIViewController, GettingLocationProtocol, SetMarkerProtocol {
+class ViewController: UIViewController, GettingLocationProtocol, SetMarkerProtocol, PlacementOfLocations{
 
 
     var locationManager = CLLocationManager()
@@ -20,27 +20,22 @@ class ViewController: UIViewController, GettingLocationProtocol, SetMarkerProtoc
     override func viewDidLoad() {
         
         let location = gettingLocation()
+        
+        
 
-        let frameOfMap = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height - 50)
-        
-        
+        let frameOfMap = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
         
         let camera = GMSCameraPosition(latitude: CLLocationDegrees(location[0].latitude), longitude: CLLocationDegrees(location[0].longitude), zoom: 15.0)
-        //let camera1 = GMSCameraPosition(target: , zoom: <#T##Float#>)
         self.mapView = GMSMapView.map(withFrame: frameOfMap, camera: camera)
         self.view.addSubview(mapView)
         
-        createAndSetMarker(CLLocationCoordinate2D(latitude: CLLocationDegrees( location[0].latitude), longitude: CLLocationDegrees(location[0].longitude)), location[0].name, self.mapView, location[0].color)
+        placementOfLocation(location, self.mapView)
         
         
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        self.view.backgroundColor = gettingLocation()[1].color
-        currentLocation = self.mapView.myLocation
         
-
-
         print(gettingLocation())
     }
     
@@ -58,24 +53,19 @@ extension ViewController: CLLocationManagerDelegate {
         self.mapView.settings.zoomGestures = true
     }
     
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        guard let location = locations.first else {
-//                    return
-//                }
-//
-//        mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 20, bearing: 0, viewingAngle: 0)
-//
-//        marker.position = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-//
-//        createAndSetMarker(CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude), "My point", self.mapView, .yellow)
-////        marker.title = "My point"
-////        marker.snippet = "Curent point"
-////        marker.map = mapView
-////        marker.icon = GMSMarker.markerImage(with: .red)
-//
-//        let coordinateKiev = CLLocation(latitude: 50.4536, longitude: 30.5164)
-//        let coordinateMy = CLLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-//        let distance = Int(coordinateMy.distance(from: coordinateKiev) ) // in meters
-//        print("distance =", Int(distance / 1000) )  // in km
-//    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+                    return
+                }
+
+        mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+
+        createAndSetMarker(CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude), "My point", self.mapView, .yellow)
+
+        let locationsForCalculation = gettingLocation()
+        let coordinateOfLocation = CLLocation(latitude: CLLocationDegrees(locationsForCalculation[0].latitude), longitude: CLLocationDegrees(locationsForCalculation[0].longitude))
+        let coordinateMy = CLLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let distance = Int(coordinateMy.distance(from: coordinateOfLocation) ) // in meters
+        print("distance =", Int(distance / 1000) )  // in km
+    }
 }
